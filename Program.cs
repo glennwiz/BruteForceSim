9591ac59c68e45f88483cdb7abd50ddf97b27c4a
Glenn Jacobsen <glenn.jacobsen@ams.no>
2023-06-08 15:36:47 +0200

    first pass, using all special chars, all lower and upper, and all numbers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,48 @@
-﻿namespace BruteForceSim;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        List<char> characterList = new List<char>();
+
+        // Add lowercase letters
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            characterList.Add(c);
+        }
+
+        // Add uppercase letters
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            characterList.Add(c);
+        }
+
+        // Add numbers
+        for (char c = '0'; c <= '9'; c++)
+        {
+            characterList.Add(c);
+        }
+
+        // Add special characters
+        string specialCharacters = "!@#$%^&*()";
+        characterList.AddRange(specialCharacters);
+
+        Console.Write("Enter the maximum length of the password: ");
+        int maxLength = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of attempts per second: ");
+        int attemptsPerSecond = int.Parse(Console.ReadLine());
+
+        int totalAttempts = (int)Math.Pow(characterList.Count, maxLength);
+        double totalTimeInSeconds = (double)totalAttempts / attemptsPerSecond;
+
+        TimeSpan timeSpan = TimeSpan.FromSeconds(totalTimeInSeconds);
+
+        Console.WriteLine($"Total attempts: {totalAttempts:N0}");
+        Console.WriteLine($"Estimated time: {timeSpan.Days} days, {timeSpan.Hours} hours, {timeSpan.Minutes} minutes, {timeSpan.Seconds} seconds.");
     }
 }
